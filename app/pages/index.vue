6bc43b9bac1930ae26e707d1e18b9af0a3f57bf9
NataliiaRosol <93214918+NataliiaRosol@users.noreply.github.com>
2025-01-07 14:36:50 +0200

    added hero section for home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,9 +3,30 @@
 </script>
 
 <template>
-  <div>
-    <h1>Home</h1>
-  </div>
+  <main>
+    <section class="bg-[#f1f1f1]">
+      <div class="container flex flex-col lg:flex-row items-center py-20 gap-10">
+        <div class="flex-1 order-2 lg:order-1 text-center lg:text-left">
+          <h1 class="text-2xl lg:text-3xl font-extrabold mb-6 text-balance">
+            Master the Kitchen with Ease: Unleash Your Inner Chef Today!
+          </h1>
+          <p class="text-lg lg:text-xl mb-8 text-balance">
+            Discover recipes helping you to find the easiest way to cook.
+          </p>
+          <UButton to="#recipes" label="Browse Recipes" />
+        </div>
+        <div class="flex-1 order-1 lg:order-2">
+          <NuxtImg
+            sizes="xs:100vw sm:667px"
+            src="/nuxt-course-hero.png"
+            format="webp"
+            densities="x1"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 
