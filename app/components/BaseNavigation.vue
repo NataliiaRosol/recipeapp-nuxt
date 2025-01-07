7e36bd83bd54523df003194cb1e60e5787717d04
NataliiaRosol <93214918+NataliiaRosol@users.noreply.github.com>
2025-01-07 14:18:14 +0200

    first commit

---
+++ app/components/BaseNavigation.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="py-4 border-b">
+    <nav class="flex container items-center">
+      <NuxtLink to="/" class="flex gap-1 items-center">
+        <NuxtImg width="56px" src="/icon-green.png" format="webp" alt="" />
+        <span class="text-xl font-bold">Nuxtcipes</span>
+      </NuxtLink>
+      <ul class="flex gap-6 ml-auto text-lg capitalize">
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
